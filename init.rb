8717be1b40f4d0ff079848d33dfd49a438054381
bilel-kedidi <bilel.kedidi@gmail.com>
2016-10-19 21:17:12 +0100

    add legend to gantt + enable to close all subtasks issue when parent issue is closed + add option to close parent issue when edit sub task

---
+++ init.rb
@@ -0,0 +1,32 @@
+Redmine::Plugin.register :redmine_improve do
+  name 'Redmine Improve plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
+
+require 'issue_patch'
+
+class Hooks < Redmine::Hook::ViewListener
+  # This just renders the partial in
+  # app/views/hooks/my_plugin/_view_issues_form_details_bottom.rhtml
+  # The contents of the context hash is made available as local variables to the partial.
+  #
+  # Additional context fields
+  #   :issue  => the issue this is edited
+  #   :f      => the form object to create additional fields
+
+  render_on :view_issues_form_details_bottom, :partial=> 'issues/check_box_close'
+
+  def controller_issues_edit_after_save(context={})
+    params = context[:params]
+    issue = context[:issue]
+    if issue.status.is_closed?
+      if params[:close_parent_issue]
+        issue.parent.update(status_id: issue.status_id)
+      end
+    end
+  end
+end
---
+++ lib/issue_patch.rb
@@ -0,0 +1,31 @@
+module SubtasksInherited
+  module IssuePatch
+    module InstanceMethods
+
+      # Returns a link for adding a new subtask to the given issue
+      def check_sub_tasks
+        if self.status_id_changed? and self.closed?
+          # self.self_and_descendants.each do |issue|
+          #   issue.status_id = self.status_id
+          #   issue.save
+          # end
+          self.self_and_descendants.update_all(status_id: self.status_id )
+        end
+      end
+    end
+
+    def self.included(receiver)
+      receiver.send :include, InstanceMethods
+
+      receiver.class_eval do
+        unloadable
+        after_update :check_sub_tasks
+      end
+    end
+  end
+end
+
+unless Issue.included_modules.include?(SubtasksInherited::IssuePatch)
+  #puts "Including module into IssuesHelper"
+  Issue.send(:include, SubtasksInherited::IssuePatch)
+end
